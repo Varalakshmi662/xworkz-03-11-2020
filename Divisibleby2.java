81d105f85fbe1520b13195f3085dadcb46a57ce1
Varalakshmi662 <73688730+Varalakshmi662@users.noreply.github.com>
2020-11-03 15:36:30 +0530

    Add files via upload

---
+++ Divisibleby2.java
@@ -0,0 +1,10 @@
+class Divisibleby2{
+public static void main(String args[]){
+int a[]={0,1,2,3,4,5,6,7,8,9};
+for(int i=0;i<=9;i++)
+{
+if(a[i]%2==0)
+System.out.println(a[i]);
+}
+}
+}
---
+++ EvenIndexValue.java
@@ -0,0 +1,9 @@
+class EvenIndexValue{
+public static void main(String args[]){
+int a[]={51,52,53,54,55,56,57,58,59,60};
+for(int i=0;i<=9;i=i+2)
+{
+System.out.println(a[i]);
+}
+}
+}
